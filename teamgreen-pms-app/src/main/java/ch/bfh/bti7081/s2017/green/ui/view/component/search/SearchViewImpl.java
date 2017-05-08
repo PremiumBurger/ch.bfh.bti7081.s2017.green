f20eb4749a14bcc423505960382d847692ca0532
S.Schmid <schms27@bfh.ch>
2017-05-08 21:30:32 +0200

    tried to style a button

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/view/component/search/SearchViewImpl.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/view/component/search/SearchViewImpl.java
@@ -1,7 +1,10 @@
 package ch.bfh.bti7081.s2017.green.ui.view.component.search;
 
+import com.sun.xml.internal.bind.v2.TODO;
 import com.vaadin.icons.VaadinIcons;
+import com.vaadin.server.Sizeable;
 import com.vaadin.ui.*;
+import com.vaadin.ui.themes.ValoTheme;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,14 +24,23 @@ public class SearchViewImpl extends FormLayout implements SearchView {
         /* Create UI components */
         searchName = new TextField();
         searchName.setValue("Search");
+
+        //TODO: Set Width of Textfield and Button
+        //searchName.setWidth(10, Unit.PIXELS);
+        //searchName.setWidth(80,Unit.PERCENTAGE);
         //searchName.setInputPrompt(“Search by keywords”);
         search = new Button("Search");
+        search.setWidth(20,Unit.PERCENTAGE);
         search.setIcon(VaadinIcons.SEARCH);
 
+        //TODO: Find out how to set a custom style/css/theme
+        search.setStyleName(ValoTheme.LAYOUT_COMPONENT_GROUP);
+        search.setStyleName(ValoTheme.BUTTON_PRIMARY);
         /* Add all the created components to the form */
         addComponent(header);
         header.addComponentsAndExpand(searchName,search);
 
+        header.setWidth(100,Unit.PERCENTAGE);
         setResponsive(true);
     }
 
