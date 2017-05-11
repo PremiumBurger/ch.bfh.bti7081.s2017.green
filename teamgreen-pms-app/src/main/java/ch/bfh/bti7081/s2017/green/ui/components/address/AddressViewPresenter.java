ff149d6b118230d06a40509ab1dc1637fc55e9c0
Simon Schmid <schms27@gmail.com>
2017-05-12 00:44:47 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/MasterPageImpl.java
    #	teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/address/AddressViewImpl.java
    #	teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/address/AddressViewPresenter.java

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/address/AddressViewPresenter.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/address/AddressViewPresenter.java
@@ -1,13 +1,18 @@
 package ch.bfh.bti7081.s2017.green.ui.components.address;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 /**
  * Handles the interaction between View and Model
  * @author schms27
  */
+@Component
 public class AddressViewPresenter implements AddressViewListener {
     private Address address;                //Model
     private AddressView addressView;        //View
 
+    @Autowired
     public AddressViewPresenter(AddressView addressView, Address address) {
         this.addressView = addressView;
         this.address = address;
