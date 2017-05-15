11f71c5d8a988696eea6ab23cc918e09368eccd1
S.Schmid <schms27@bfh.ch>
2017-05-15 19:48:02 +0200

    Appointment State implementation started

---
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/domain/AppointmentStateIdle.java
@@ -0,0 +1,12 @@
+package ch.bfh.bti7081.s2017.green.domain;
+
+/**
+ * Created by S.Schmid on 15.05.2017.
+ */
+public class AppointmentStateIdle extends AppointmentState {
+
+    @Override
+    public void handleEvent1() {
+
+    }
+}
