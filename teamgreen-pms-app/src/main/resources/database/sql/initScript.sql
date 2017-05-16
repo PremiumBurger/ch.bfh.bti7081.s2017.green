fb641b3435c914918cd1ab50da398d7ba86481bd
Lukas Läderach <lukas.laederach@3volutions.ch>
2017-05-16 20:36:33 +0200

    add journal and journalentry to schema

--- teamgreen-pms-app/src/main/resources/database/sql/initScript.sql
+++ teamgreen-pms-app/src/main/resources/database/sql/initScript.sql
@@ -2,13 +2,24 @@
 --  Update Database Script
 --  *********************************************************************
 --  Change Log: src/main/resources/database/all.changelog-master.xml
---  Ran at: 11/05/17 18:06
+--  Ran at: 16.05.17 20:59
 --  Against: root@localhost@jdbc:mysql://localhost:3306/teamgreen?serverTimezone=UTC&nullNamePatternMatchesAll=true&useSSL=false
 --  Liquibase version: 3.5.3
 --  *********************************************************************
 
+--  Create Database Lock Table
+CREATE TABLE DATABASECHANGELOGLOCK (ID INT NOT NULL, LOCKED BIT(1) NOT NULL, LOCKGRANTED datetime NULL, LOCKEDBY VARCHAR(255) NULL, CONSTRAINT PK_DATABASECHANGELOGLOCK PRIMARY KEY (ID));
+
+--  Initialize Database Lock Table
+DELETE FROM DATABASECHANGELOGLOCK;
+
+INSERT INTO DATABASECHANGELOGLOCK (ID, LOCKED) VALUES (1, 0);
+
 --  Lock Database
-UPDATE DATABASECHANGELOGLOCK SET LOCKED = 1, LOCKEDBY = '192.168.154.129 (192.168.154.129)', LOCKGRANTED = '2017-05-11 18:06:25.085' WHERE ID = 1 AND LOCKED = 0;
+UPDATE DATABASECHANGELOGLOCK SET LOCKED = 1, LOCKEDBY = 'DESKTOP-TLLCFAS (147.87.41.135)', LOCKGRANTED = '2017-05-16 20:59:37.707' WHERE ID = 1 AND LOCKED = 0;
+
+--  Create Database Change Log Table
+CREATE TABLE DATABASECHANGELOG (ID VARCHAR(255) NOT NULL, AUTHOR VARCHAR(255) NOT NULL, FILENAME VARCHAR(255) NOT NULL, DATEEXECUTED datetime NOT NULL, ORDEREXECUTED INT NOT NULL, EXECTYPE VARCHAR(10) NOT NULL, MD5SUM VARCHAR(35) NULL, DESCRIPTION VARCHAR(255) NULL, COMMENTS VARCHAR(255) NULL, TAG VARCHAR(255) NULL, LIQUIBASE VARCHAR(20) NULL, CONTEXTS VARCHAR(255) NULL, LABELS VARCHAR(255) NULL, DEPLOYMENT_ID VARCHAR(10) NULL);
 
 --  Changeset src/main/resources/database/schema.changelog-master.xml::1::vonac1
 CREATE TABLE GenderType (id BIGINT AUTO_INCREMENT NOT NULL, description VARCHAR(50) NULL, CONSTRAINT PK_GENDERTYPE PRIMARY KEY (id));
@@ -17,20 +28,24 @@ CREATE TABLE Address (id BIGINT AUTO_INCREMENT NOT NULL, strasse VARCHAR(255) NU
 
 CREATE TABLE Person (id BIGINT AUTO_INCREMENT NOT NULL, personType VARCHAR(50) NOT NULL, addressId BIGINT NULL, genderTypeId BIGINT NOT NULL, firstName VARCHAR(255) NULL, lastName VARCHAR(255) NULL, birthDate date NULL, phone VARCHAR(50) NULL, mobile VARCHAR(50) NULL, ahvNr VARCHAR(16) NULL, email VARCHAR(255) NULL, CONSTRAINT PK_PERSON PRIMARY KEY (id), CONSTRAINT fk_person_address FOREIGN KEY (addressId) REFERENCES Address(id), CONSTRAINT fk_person_gender FOREIGN KEY (genderTypeId) REFERENCES GenderType(id));
 
-CREATE TABLE Alarm (id BIGINT AUTO_INCREMENT NOT NULL, healthVisitorId BIGINT NOT NULL, patientId BIGINT NULL, coordinates VARCHAR(255) NULL, timestamp timestamp NULL, CONSTRAINT PK_ALARM PRIMARY KEY (id), CONSTRAINT fk_alarm_patient FOREIGN KEY (patientId) REFERENCES Person(id), CONSTRAINT fk_alarm_healthvisitor FOREIGN KEY (healthVisitorId) REFERENCES Person(id));
+CREATE TABLE Alarm (id BIGINT AUTO_INCREMENT NOT NULL, healthVisitorId BIGINT NOT NULL, patientId BIGINT NULL, coordinates VARCHAR(255) NOT NULL, timestamp timestamp NOT NULL, CONSTRAINT PK_ALARM PRIMARY KEY (id), CONSTRAINT fk_alarm_patient FOREIGN KEY (patientId) REFERENCES Person(id), CONSTRAINT fk_alarm_healthvisitor FOREIGN KEY (healthVisitorId) REFERENCES Person(id));
 
-CREATE TABLE Appointment (id BIGINT AUTO_INCREMENT NOT NULL, healthVisitorId BIGINT NOT NULL, patientId BIGINT NULL, intendedDateTime timestamp NULL, CONSTRAINT PK_APPOINTMENT PRIMARY KEY (id), CONSTRAINT fk_alarm_healthvisitor FOREIGN KEY (healthVisitorId) REFERENCES Person(id), CONSTRAINT fk_alarm_patient FOREIGN KEY (patientId) REFERENCES Person(id));
+CREATE TABLE Appointment (id BIGINT AUTO_INCREMENT NOT NULL, healthVisitorId BIGINT NOT NULL, patientId BIGINT NULL, `from` timestamp NOT NULL, `to` timestamp NOT NULL, CONSTRAINT PK_APPOINTMENT PRIMARY KEY (id), CONSTRAINT fk_appointment_healthvisitor FOREIGN KEY (healthVisitorId) REFERENCES Person(id), CONSTRAINT fk_appointment_patient FOREIGN KEY (patientId) REFERENCES Person(id));
 
-CREATE TABLE PatientHealthVisitor (patientId BIGINT NOT NULL, healthVisitorId BIGINT NOT NULL, CONSTRAINT PK_PATIENTHEALTHVISITOR PRIMARY KEY (patientId, healthVisitorId), CONSTRAINT fk_pat_heal_patient FOREIGN KEY (patientId) REFERENCES Person(id), CONSTRAINT fk_pat_heal_healthvisitor FOREIGN KEY (healthVisitorId) REFERENCES Person(id));
+CREATE TABLE Journal (id BIGINT AUTO_INCREMENT NOT NULL, patientId BIGINT NULL, createdOn timestamp NOT NULL, CONSTRAINT PK_JOURNAL PRIMARY KEY (id), CONSTRAINT fk_journal_patient FOREIGN KEY (patientId) REFERENCES Person(id));
 
-INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('1', 'vonac1', 'src/main/resources/database/schema.changelog-master.xml', NOW(), 1, '7:e12ff7b8106600e2fabd988e67ba4a5b', 'createTable tableName=GenderType; createTable tableName=Address; createTable tableName=Person; createTable tableName=Alarm; createTable tableName=Appointment; createTable tableName=PatientHealthVisitor', '', 'EXECUTED', NULL, NULL, '3.5.3', '4518786755');
+CREATE TABLE JournalEntry (id BIGINT AUTO_INCREMENT NOT NULL, journalEntryType BIGINT NOT NULL, journalId INT NOT NULL, text VARCHAR(2000) NULL, isImportant BIT(1) NOT NULL, appointmentId BIGINT NULL, createdBy BIGINT NOT NULL, createdOn timestamp NOT NULL, modifiedOn timestamp NULL, CONSTRAINT PK_JOURNALENTRY PRIMARY KEY (id), CONSTRAINT fk_journalentry_person FOREIGN KEY (createdBy) REFERENCES Person(id), CONSTRAINT fk_journalentry_journal FOREIGN KEY (journalId) REFERENCES Journal(id), CONSTRAINT fk_journalentry_appointment FOREIGN KEY (appointmentId) REFERENCES Appointment(id));
+
+CREATE TABLE PatientHealthVisitor (patientId BIGINT NOT NULL, healthVisitorId BIGINT NOT NULL, CONSTRAINT PK_PATIENTHEALTHVISITOR PRIMARY KEY (patientId, healthVisitorId), CONSTRAINT fk_pat_heal_healthvisitor FOREIGN KEY (healthVisitorId) REFERENCES Person(id), CONSTRAINT fk_pat_heal_patient FOREIGN KEY (patientId) REFERENCES Person(id));
+
+INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('1', 'vonac1', 'src/main/resources/database/schema.changelog-master.xml', NOW(), 1, '7:d8638f098c69132059d6a649745ecdb8', 'createTable tableName=GenderType; createTable tableName=Address; createTable tableName=Person; createTable tableName=Alarm; createTable tableName=Appointment; createTable tableName=Journal; createTable tableName=JournalEntry; createTable tableName...', '', 'EXECUTED', NULL, NULL, '3.5.3', '4961178324');
 
 --  Changeset src/main/resources/database/basedata.changelog-master.xml::basedata::vonac1
 INSERT INTO GenderType (id, description) VALUES ('1', 'Male');
 
 INSERT INTO GenderType (id, description) VALUES ('2', 'Female');
 
-INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('basedata', 'vonac1', 'src/main/resources/database/basedata.changelog-master.xml', NOW(), 2, '7:36dae9cb949d5cff4e7b6315d3bf7dae', 'insert tableName=GenderType; insert tableName=GenderType', '', 'EXECUTED', NULL, NULL, '3.5.3', '4518786755');
+INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('basedata', 'vonac1', 'src/main/resources/database/basedata.changelog-master.xml', NOW(), 2, '7:36dae9cb949d5cff4e7b6315d3bf7dae', 'insert tableName=GenderType; insert tableName=GenderType', '', 'EXECUTED', NULL, NULL, '3.5.3', '4961178324');
 
 --  Changeset src/main/resources/database/testdata.changelog-master.xml::testdata::vonac1
 INSERT INTO Address (id, strasse, plz, city, country) VALUES ('1', 'Wankdorffeldstrasse 5', '3000', 'Bern', 'CH');
@@ -41,7 +56,14 @@ INSERT INTO Address (id, strasse, plz, city, country) VALUES ('3', 'Postweg 17',
 
 INSERT INTO Person (id, personType, addressId, genderTypeId, firstName, lastName, birthDate, phone, mobile, ahvNr, email) VALUES ('1', 'healthvisitor', '1', '2', 'Sabine', 'Pitex', '2017-08-15', '077 777 77 77', '088 888 88 88', '756.2831.3945.39', 'sabi@yeah.ch');
 
-INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('testdata', 'vonac1', 'src/main/resources/database/testdata.changelog-master.xml', NOW(), 3, '7:d7e1dc33ecd35476ab74eace5331e765', 'insert tableName=Address; insert tableName=Address; insert tableName=Address; insert tableName=Person', '', 'EXECUTED', NULL, NULL, '3.5.3', '4518786755');
+INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('testdata', 'vonac1', 'src/main/resources/database/testdata.changelog-master.xml', NOW(), 3, '7:d7e1dc33ecd35476ab74eace5331e765', 'insert tableName=Address; insert tableName=Address; insert tableName=Address; insert tableName=Person', '', 'EXECUTED', NULL, NULL, '3.5.3', '4961178324');
+
+--  Changeset src/main/resources/database/testdata.changelog-master.xml::testdata_2::schms27
+INSERT INTO Person (id, personType, addressId, genderTypeId, firstName, lastName, birthDate, phone, mobile, ahvNr, email) VALUES ('2', 'patient', '3', '1', 'Beat', 'Orderline', '1980-08-15', '077 555 55 25', '088 544 54 25', '756.2821.3225.39', 'beat@mail.ch');
+
+INSERT INTO Appointment (id, healthVisitorId, patientId, intendedDateTime) VALUES ('1', '1', '2', '2017-10-30 08:00:00');
+
+INSERT INTO DATABASECHANGELOG (ID, AUTHOR, FILENAME, DATEEXECUTED, ORDEREXECUTED, MD5SUM, DESCRIPTION, COMMENTS, EXECTYPE, CONTEXTS, LABELS, LIQUIBASE, DEPLOYMENT_ID) VALUES ('testdata_2', 'schms27', 'src/main/resources/database/testdata.changelog-master.xml', NOW(), 4, '7:32d5064855f560f5b0c544170b68e795', 'insert tableName=Person; insert tableName=Appointment', '', 'EXECUTED', NULL, NULL, '3.5.3', '4961178324');
 
 --  Release Database Lock
 UPDATE DATABASECHANGELOGLOCK SET LOCKED = 0, LOCKEDBY = NULL, LOCKGRANTED = NULL WHERE ID = 1;
