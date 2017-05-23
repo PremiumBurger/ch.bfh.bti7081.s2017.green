292aa4515c93a78497af3c6c64a7d43cfce18dd2
der-tobi <der-tobi@gmx.ch>
2017-05-23 21:26:37 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/domain/Appointment.java
    #	teamgreen-pms-app/src/main/resources/database/schema.changelog-master.xml
    #	teamgreen-pms/src/main/resources/doc/cs01/task13/task13.html
    #	teamgreen-pms/src/main/resources/doc/diary.html

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/bean/AppointmentJournalEntryBean.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/bean/AppointmentJournalEntryBean.java
@@ -3,5 +3,5 @@ package ch.bfh.bti7081.s2017.green.bean;
 /**
  * Created by Tobias Joder on 23.05.2017.
  */
-public class JournalEntryBean {
+public class AppointmentJournalEntryBean {
 }
---
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/domain/builder/AppointmentJournalEntryBuilder.java
@@ -0,0 +1,7 @@
+package ch.bfh.bti7081.s2017.green.domain.builder;
+
+/**
+ * Created by Tobias Joder on 22.05.2017.
+ */
+public class AppointmentJournalEntryBuilder {
+}
