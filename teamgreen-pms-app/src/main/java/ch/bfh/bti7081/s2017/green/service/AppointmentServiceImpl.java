ba64636f472e7bcc48aaa296b13496b6891f79ef
simon.schmid <schms27@gmail.com>
2017-05-30 16:16:55 +0200

    changed appointmentService to trigger state-dependent events

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/service/AppointmentServiceImpl.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/service/AppointmentServiceImpl.java
@@ -1,6 +1,7 @@
 package ch.bfh.bti7081.s2017.green.service;
 
 import ch.bfh.bti7081.s2017.green.bean.AppointmentBean;
+import ch.bfh.bti7081.s2017.green.bean.AppointmentStateTypeBean;
 import ch.bfh.bti7081.s2017.green.data.AppointmentRepository;
 import ch.bfh.bti7081.s2017.green.domain.Appointment;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,6 +18,20 @@ public class AppointmentServiceImpl extends BaseService<Appointment, Appointment
     @Override
     protected Class<AppointmentBean> getType() {
         return AppointmentBean.class;
+    }
+
+    /**
+     * Overrides save Method from BaseService
+     * to trigger the state-dependent Events
+     * @param appointmentBean
+     */
+    public long save(AppointmentBean appointmentBean) {
+        AppointmentStateTypeBean oldAppointmentStateTypeBean = this.getOne(appointmentBean.getId()).getAppointmentStateType();
+        AppointmentStateTypeBean newAppointmentStateTypeBean = appointmentBean.getAppointmentStateType();
+        if(newAppointmentStateTypeBean != oldAppointmentStateTypeBean){
+            newAppointmentStateTypeBean.onStateSet(appointmentBean);
+        }
+        return super.save(appointmentBean);
     }
 
 }
