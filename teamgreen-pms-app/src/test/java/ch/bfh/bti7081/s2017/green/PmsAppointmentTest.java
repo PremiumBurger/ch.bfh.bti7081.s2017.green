ab65e65f73db00c9687dd7dc4daa4ddd2c8bf1a2
Simon Schmid <schms27@gmail.com>
2017-06-11 17:56:49 +0200

    changed unit test for appointment service

--- teamgreen-pms-app/src/test/java/ch/bfh/bti7081/s2017/green/PmsAppointmentTest.java
+++ teamgreen-pms-app/src/test/java/ch/bfh/bti7081/s2017/green/PmsAppointmentTest.java
@@ -1,22 +1,19 @@
 package ch.bfh.bti7081.s2017.green;
 
 import ch.bfh.bti7081.s2017.green.bean.AppointmentBean;
-import ch.bfh.bti7081.s2017.green.bean.AppointmentStateTypeBean;
+import ch.bfh.bti7081.s2017.green.data.AppointmentRepository;
 import ch.bfh.bti7081.s2017.green.domain.Appointment;
-import ch.bfh.bti7081.s2017.green.domain.AppointmentStateType;
 import ch.bfh.bti7081.s2017.green.domain.builder.AppointmentBuilder;
 import ch.bfh.bti7081.s2017.green.service.AppointmentServiceImpl;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.runners.MockitoJUnitRunner;
-import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import javax.annotation.Resource;
+import java.util.HashSet;
 import java.util.Set;
 
 
@@ -24,46 +21,48 @@ import java.util.Set;
  * Tests the Appointment
  * @author schms27
  */
-//@RunWith(MockitoJUnitRunner.class)
-//@RunWith(SpringJUnit4ClassRunner.class)
-//@ContextConfiguration({"/application.properties"})
+@RunWith(MockitoJUnitRunner.class)
 public class PmsAppointmentTest {
 
-    //Todo:Find out how to mock service
-    //@InjectMocks
-    /*@Resource
-    private Appointment appointment;
-
-    @Resource
     @Mock
-    private AppointmentServiceImpl service;*/
+    private AppointmentRepository repository;
 
+    private AppointmentServiceImpl service;
+
+    Set<Appointment> allAppointments = new HashSet<Appointment>();
+    Appointment appointmentDomainModel;
 
     @Before
     public void setUp() throws Exception {
+        appointmentDomainModel = AppointmentBuilder.anAppointment().rundumSorglos().build();
 
+        //Set Attributes which normally get set by db
+        appointmentDomainModel.setId(1);
+        appointmentDomainModel.getAppointmentStateType().setDescription("New");
+
+        allAppointments.add(appointmentDomainModel);
+        service = new AppointmentServiceImpl(repository);
+
+        //Mock repos functionality
+        Mockito.when(repository.findAll()).thenReturn(() -> allAppointments.iterator());
+        Mockito.when(repository.findOne((long)1)).thenReturn(appointmentDomainModel);
+        Mockito.when(repository.save(appointmentDomainModel)).thenReturn(appointmentDomainModel);
     }
 
     @Test
-    public void TestAppointmentEntityBean() {
+    public void TestAppointmentService() {
         // Arrange
-        //Set<AppointmentBean> testBeanList = service.getAll();
+        Set<AppointmentBean> allAppointmentBeans = service.getAll();
+        AppointmentBean bean = service.getOne(1);
 
-
-
-        Appointment appointment = AppointmentBuilder.anAppointment().rundumSorglos().build();
-        AppointmentBean appointmentBean = new AppointmentBean();
-
-
-        // Act
-        appointmentBean.setEntity(appointment, true);
-
-        AppointmentStateType type1 = appointment.getAppointmentStateType();
-        AppointmentStateTypeBean type2 = appointmentBean.getAppointmentStateType();
 
         // Assert
-        //Assert.assertEquals(type2.getAppointmentState().getDescription(),"NEW");
+        Assert.assertTrue(service.getOne(1)!=null);
+        Assert.assertEquals(service.getOne(1).getId(),bean.getId());
+        Assert.assertEquals(allAppointments.size(),allAppointmentBeans.size());
+        Assert.assertTrue(bean.getHealthVisitor().getLastName().equals(appointmentDomainModel.getHealthVisitor().getLastName()));
+        Assert.assertTrue(bean.getAppointmentStateType().getDescription().equals(appointmentDomainModel.getAppointmentStateType().getDescription()));
+        Assert.assertTrue(bean.getFrom().equals(appointmentDomainModel.getFrom()));
+        Assert.assertEquals(service.save(bean),(long)1);
     }
-
-
 }
