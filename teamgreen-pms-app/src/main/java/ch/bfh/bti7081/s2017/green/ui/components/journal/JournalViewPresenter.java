022b4df5190bfc583faa2345ed8ac1989dae7a8c
der-tobi <der-tobi@gmx.ch>
2017-06-12 16:33:04 +0200

    jabüaosdb

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/journal/JournalViewPresenter.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/journal/JournalViewPresenter.java
@@ -1,6 +1,7 @@
 package ch.bfh.bti7081.s2017.green.ui.components.journal;
 
-import ch.bfh.bti7081.s2017.green.bean.PatientBean;
+import ch.bfh.bti7081.s2017.green.bean.AppointmentJournalEntryBean;
+import ch.bfh.bti7081.s2017.green
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,10 +12,15 @@ public class JournalViewPresenter implements JournalViewListener {
     private JournalView journalView;
 
     @Autowired
-    public JournalViewPresenter(JournalView journalView, JournalModel journalModel) {
+    public JournalViewPresenter(JournalView journalView, JournalModel journalModel, EventBus eventBus) {
         this.journalModel = journalModel;
         this.journalView = journalView;
         journalView.addListener(this);
+
+    }
+
+    public void onAddJournalEntryButtonClick(){
+        AppointmentJournalEntryBean appointmentJournalEntryBean = new AppointmentJournalEntryBean();
 
     }
 
