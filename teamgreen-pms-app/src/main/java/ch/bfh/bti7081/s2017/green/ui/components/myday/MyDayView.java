b6f4c8aa2ad5644ec52f9b9de8c1e8768b0fe520
Joris Baiutti <joris.baiutti@gmail.com>
2017-06-12 19:30:14 +0200

    Commenting MyDay

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/myday/MyDayView.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/myday/MyDayView.java
@@ -2,11 +2,12 @@ package ch.bfh.bti7081.s2017.green.ui.components.myday;
 
 import ch.bfh.bti7081.s2017.green.bean.AppointmentBean;
 import com.vaadin.navigator.View;
-
 import java.util.List;
 
+/**
+ * Inherits from Vaadin Navigator View and defines which methods are available on the View Implementation
+ */
 public interface MyDayView extends View {
     void addListener(MyDayViewListener viewListener);
-
     void init(List<AppointmentBean> appointments);
 }
