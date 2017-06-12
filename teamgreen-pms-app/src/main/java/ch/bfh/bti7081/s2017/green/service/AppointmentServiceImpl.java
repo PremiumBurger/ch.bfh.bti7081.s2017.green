0342f176002a0cac016a6d1e2c63157836335b45
cnvonallmen@hotmail.com <sokrates91>
2017-06-13 00:40:29 +0200

    add google maps to appointment detail

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/service/AppointmentServiceImpl.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/service/AppointmentServiceImpl.java
@@ -3,16 +3,34 @@ package ch.bfh.bti7081.s2017.green.service;
 import ch.bfh.bti7081.s2017.green.bean.AppointmentBean;
 import ch.bfh.bti7081.s2017.green.bean.AppointmentStateTypeBean;
 import ch.bfh.bti7081.s2017.green.data.AppointmentRepository;
+import ch.bfh.bti7081.s2017.green.domain.Address;
 import ch.bfh.bti7081.s2017.green.domain.Appointment;
+import ch.bfh.bti7081.s2017.green.webservice.GoogleGeocodingWebService;
+import ch.bfh.bti7081.s2017.green.webservice.dto.Geometry;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AppointmentServiceImpl extends BaseService<Appointment, AppointmentBean, AppointmentRepository> implements AppointmentService {
 
+    private GoogleGeocodingWebService googleGeocodingWebService;
+
     @Autowired
-    public AppointmentServiceImpl(AppointmentRepository repository) {
+    public AppointmentServiceImpl(AppointmentRepository repository, GoogleGeocodingWebService googleGeocodingWebService) {
         super(repository);
+        this.googleGeocodingWebService = googleGeocodingWebService;
+
+
+        Address addr = new Address();
+        addr.setStrasse("Metzgergasse 1");
+        addr.setPlz("3400");
+        addr.setCity("Burgdorf");
+
+        Geometry coordinatesByAddress = googleGeocodingWebService.getCoordinatesByAddress(addr);
+
+        String test = "";
+
+
     }
 
     @Override
