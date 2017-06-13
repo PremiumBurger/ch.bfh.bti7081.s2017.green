5b7e69bd5bfe6316020d086b984335fbb41fe36f
Joris Baiutti <joris.baiutti@gmail.com>
2017-06-13 19:01:49 +0200

    Refactoring and Commenting Patients Overview

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/myday/MyDayPresenter.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/myday/MyDayPresenter.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Component;
 public class MyDayPresenter implements MyDayViewListener {
 
     /**
-     * Declaration of myDayView
+     * Declaration of MyDay
      * Access to MyDay Model
      */
     private MyDay myDay;
--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/patients/PatientView.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/patients/PatientView.java
@@ -1,12 +1,11 @@
 package ch.bfh.bti7081.s2017.green.ui.components.patients;
 
 import ch.bfh.bti7081.s2017.green.bean.PatientBean;
-
 import java.util.List;
 import com.vaadin.navigator.View;
 
 /**
- * Created by joris on 01.06.17.
+ * Inherits from Vaadin Navigator View and defines which methods are available on the View Implementation
  */
 public interface PatientView extends View{
     public void addListener(PatientViewListener listener);
