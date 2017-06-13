f13ef21aecae34ac0bfe65250b6215626bc4732e
Mathew Thekkekara <mathew.thekkekara@realstuff.ch>
2017-06-13 18:34:01 +0200

    joris hets wöue

--- teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/PatientDetail/PatientDetailViewImpl.java
+++ teamgreen-pms-app/src/main/java/ch/bfh/bti7081/s2017/green/ui/components/PatientDetail/PatientDetailViewImpl.java
@@ -1,14 +1,19 @@
 package ch.bfh.bti7081.s2017.green.ui.components.PatientDetail;
 
 import ch.bfh.bti7081.s2017.green.bean.AppointmentBean;
+import ch.bfh.bti7081.s2017.green.bean.HealthVisitorBean;
 import ch.bfh.bti7081.s2017.green.bean.PatientBean;
 import ch.bfh.bti7081.s2017.green.ui.components.autcomplete.Autocomplete;
 import ch.bfh.bti7081.s2017.green.ui.stub.PmsDummyImages;
-import com.vaadin.data.Binder;
+import ch.bfh.bti7081.s2017.green.util.PmsConstants;
 import com.vaadin.navigator.ViewChangeListener;
+import com.vaadin.ui.ComboBox;
+import com.vaadin.ui.Label;
 import org.springframework.stereotype.Component;
 import com.vaadin.ui.*;
+import org.vaadin.addons.stackpanel.StackPanel;
 
+import java.time.format.DateTimeFormatter;
 import java.util.List;
 
 /**
@@ -36,6 +41,11 @@ public class PatientDetailViewImpl extends VerticalLayout implements PatientDeta
         removeAllComponents();
 
         addComponent(buildPatientDetail());
+        addComponent(buildPatientsAppointments());
+        addComponent(buidPatientJournal());
+        addComponent(buildPromisses());
+        addComponent(buildEmergency());
+        addComponent(buildEmergencyBox());
     }
 
     @Override
@@ -62,33 +72,104 @@ public class PatientDetailViewImpl extends VerticalLayout implements PatientDeta
     }
 
     private Panel buildPatientDetail() {
-        PatientBean patModel = this.patientBean;
+        Panel panel = new Panel( patientBean.getFirstName()+" "+patientBean.getLastName() +"(#"+patientBean.getId()+")");
+        HorizontalLayout horizontal = new HorizontalLayout();
+        VerticalLayout vertical = new VerticalLayout();
 
-        // layout
-        Panel patientDetails = new Panel("Patient (#" + patModel.getId() + ")");
-        HorizontalLayout horizontalLayout = new HorizontalLayout();
-        horizontalLayout.setSpacing(true);
-        horizontalLayout.setMargin(true);
-        FormLayout patForm = new FormLayout();
-        horizontalLayout.addComponents(PmsDummyImages.getPatientImage(), patForm);
-        patForm.setMargin(true);
-        patientDetails.setContent(horizontalLayout);
+        //components
+        Label name = new Label("Name");
+        Label ahv = new Label("AHV Nr.");
+        Label address = new Label("Address");
+        Label tel = new Label("Telefon");
+        Grid<HealthVisitorBean> hv = new Grid<>();
 
-        TextField firstName = new TextField("First Name");
-        TextField lastName = new TextField("Last Name");
-        firstName.setEnabled(false);
-        lastName.setEnabled(false);
-        patForm.addComponents(firstName, lastName);
+        //componentStyles
+        hv.setWidth(100,Unit.PERCENTAGE);
 
-        // bindings
-        Binder<PatientBean> binder = new Binder<>(PatientBean.class);
-        binder.forField(firstName).bind("firstName");
-        binder.forField(lastName).bind("lastName");
-        binder.setBean(patModel);
+        //Caption Components
+        name.setCaption("Name :");
+        ahv.setCaption("AHV Nr. :");
+        address.setCaption("Address :");
+        tel.setCaption("Telefon :");
+        hv.setCaption("Healthvisitor");
+
+        //values of components
+        name.setValue(patientBean.getFullName());
+        ahv.setValue(patientBean.getAhvNr());
+        address.setValue(patientBean.getAddress().getStrasse()+" "+patientBean.getAddress().getPlz()+" "+patientBean.getAddress().getCity());
+        tel.setValue(patientBean.getPhone());
+        hv.setItems(patientBean.getHealthVisitors());
+        hv.addColumn(HealthVisitorBean::getName).setCaption("Name");
+
+        //add labels to component
+        vertical.addComponents(name,ahv,address,tel,hv);
+
+        horizontal.addComponent(vertical);
+        horizontal.addComponent(PmsDummyImages.getPatientImage());
+        panel.setContent(horizontal);
+        StackPanel.extend(panel);
+
 
         // styling
-        patientDetails.setResponsive(true);
-        patientDetails.setWidth(100, Unit.PERCENTAGE);
-        return patientDetails;
+        panel.setResponsive(true);
+        panel.setWidth(100, Unit.PERCENTAGE);
+        return panel;
+    }
+
+    private Panel buildPatientsAppointments() {
+        DateTimeFormatter format = DateTimeFormatter.ofPattern(PmsConstants.SWISS_DATE_FORMAT);
+        Panel panel = new Panel("Appointments list");
+        Grid<AppointmentBean> grid = new Grid<>();
+        grid.setCaption("Appointments");
+        grid.setItems(patientBean.getAppointments());
+        grid.addColumn(app -> app.getFrom().format(format)).setCaption("From");
+        grid.addColumn(app -> app.getTo().format(format)).setCaption("To");
+        grid.addColumn(app -> app.getAppointmentStateType().getDescription()).setCaption("State");
+
+        //render a Button that navigates to Appointment Detail
+        grid.addSelectionListener(e -> {
+            getUI().getNavigator().navigateTo("AppointmentDetail" + "/" + e.getFirstSelectedItem().get().getId());
+        });
+
+        //style of Grid
+        grid.setWidth(100,Unit.PERCENTAGE);
+        grid.setResponsive(true);
+
+        panel.setContent(grid);
+        //TODO medication anzeigen ?
+        StackPanel.extend(panel);
+
+        return panel;
+    }
+
+    private Panel buidPatientJournal() {
+        Panel panel = new Panel("Journal");
+        //TODO unterer Code auskomentieren sobald JournalEntryListComponent übernommen
+        //com.vaadin.ui.Component journal = addComponent(new JournalEntryListComponent(patientBean.getJournal().getJournalEntries()));
+        //panel.setContent(journal);
+
+        StackPanel.extend(panel);
+        return panel;
+    }
+
+    private Panel buildPromisses() {
+        Panel promisses = new Panel("Promisses");
+        StackPanel.extend(promisses);
+        return promisses;
+
+    }
+
+    private Panel buildEmergency() {
+        Panel emergency = new Panel("emergency");
+        StackPanel.extend(emergency);
+        return emergency;
+    }
+
+    private Panel buildEmergencyBox() {
+        Panel emergencyBox = new Panel("Emergency Box");
+        StackPanel.extend(emergencyBox);
+        return emergencyBox;
     }
 }
+
+
